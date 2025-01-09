7ab13fdf93cd013eedb6140d1fe7e3c89d7fca80
Lostjerome <naronkrach@gmail.com>
2025-01-09 14:48:15 +0700

    chore: Add @vercel/speed-insights for website performance monitoring

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SmoothScrolling from "@/app/components/SmoothScrolling";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <SmoothScrolling>{children}</SmoothScrolling>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
